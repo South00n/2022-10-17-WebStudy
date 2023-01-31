2323eb674add0af579bb0bac447a064161e39286
user <nhk828@naver.com>
2023-01-31 12:53:52 +0900

    게시판 댓글

--- JSPFinalProject/src/main/java/com/sist/dao/FreeBoardDAO.java
+++ JSPFinalProject/src/main/java/com/sist/dao/FreeBoardDAO.java
@@ -2,6 +2,7 @@ package com.sist.dao;
 
 import java.util.*;
 
+import com.sist.vo.BoardReplyVO;
 import com.sist.vo.FreeBoardVO;
 
 import java.sql.*;
@@ -138,8 +139,149 @@ public class FreeBoardDAO {
 		return vo;
 	}
 	// 수정
-	// 삭제
-	// 검색 => 다중 검색
-	// 댓글 ===> 프로시저
+	public FreeBoardVO boardUpdateData(int no) {
+		FreeBoardVO vo = new FreeBoardVO();
+		try {
+			conn = CreateConnnection.getConnection();
+			// 조회수 증가
+			String sql = "SELECT no, name, subject, content "
+				+ "FROM project_freeboard "
+				+ "WHERE no = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, no); // IN OUT
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			vo.setNo(rs.getInt(1));
+			vo.setName(rs.getString(2));
+			vo.setSubject(rs.getString(3));
+			vo.setContent(rs.getString(4));
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			CreateConnnection.disConnection(conn, ps);
+		}
+		return vo;
+	}
 	
+	public boolean boardUpdate(FreeBoardVO vo) {
+		boolean bCheck = false;
+		try {
+			conn = CreateConnnection.getConnection();
+			String sql = "SELECT pwd FROM project_freeboard "
+					+ "WHERE no = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, vo.getNo());
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			String db_pwd = rs.getString(1);
+			rs.close();
+			if(db_pwd.equals(vo.getPwd())) {
+				bCheck = true;
+				sql = "UPDATE project_freeboard SET "
+						+ "name = ?, subject = ?, content = ? "
+						+ "WHERE no = ?";
+				ps = conn.prepareStatement(sql);
+				ps.setString(1, vo.getName());
+				ps.setString(2, vo.getSubject());
+				ps.setString(3, vo.getContent());
+				ps.setInt(4, vo.getNo());
+				ps.executeUpdate();
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			CreateConnnection.disConnection(conn, ps);
+		}
+		return bCheck;
+	}
+	// 삭제
+	public boolean boardDelete(int no, String pwd) {
+		boolean bCheck = false;
+		try {
+			conn = CreateConnnection.getConnection();
+			String sql = "SELECT pwd FROM project_freeboard "
+					+ "WHERE no = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, no);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			String db_pwd = rs.getString(1);
+			rs.close();
+			if(db_pwd.equals(pwd)) {
+				bCheck = true;
+				sql = "DELETE FROM project_freeboard "
+					+ "WHERE no = ?";
+				ps = conn.prepareStatement(sql);
+				ps.setInt(1, no);
+				ps.executeUpdate();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			CreateConnnection.disConnection(conn, ps);
+		}
+		
+		return bCheck;
+	}
+	
+	// 검색 => 다중 검색
+	// 댓글 pr_rno_seq
+	public void replyInsert(BoardReplyVO vo) {
+		try {
+			conn = CreateConnnection.getConnection();
+			String sql = "INSERT INTO project_reply(rno, bno, id, name, msg, group_id) "
+						+ "VALUES(pr_rno_seq.nextval, ?, ?, ?, ?, (SELECT NVL(MAX(group_id)+1,1) FROM project_reply))";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, vo.getBno());
+			ps.setString(2, vo.getId());
+			ps.setString(3, vo.getName());
+			ps.setString(4, vo.getMsg());
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			CreateConnnection.disConnection(conn, ps);
+		}
+	}
+	// 댓글 읽기
+	public List<BoardReplyVO> replyListData(int bno) {
+		List<BoardReplyVO> list = new ArrayList<BoardReplyVO>();
+		try {
+			conn=CreateConnnection.getConnection();
+			String sql = "SELECT rno, bno, id, name, msg, group_tab, TO_CHAR(regdate, 'YYYY-MM-DD HH24:MI:SS') "
+						+ "FROM project_reply "
+						+ "WHERE bno = ? " // 게시물에 해당되는 댓글을 가져와라
+						+ "ORDER BY group_id DESC, group_step ASC";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, bno);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				BoardReplyVO vo = new BoardReplyVO();
+				vo.setRno(rs.getInt(1));
+				vo.setBno(rs.getInt(2));
+				vo.setId(rs.getString(3));
+				vo.setName(rs.getString(4));
+				vo.setMsg(rs.getString(5));
+				vo.setGroup_tab(rs.getInt(6));
+				vo.setDbday(rs.getString(7));
+				list.add(vo);
+			}
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			CreateConnnection.disConnection(conn, ps);
+		}
+		
+		return list;
+	}
 }
+
+
+
+
+
+
+
