fe6248501c8282c9e247b9c0b618b139b59f4c12
user <nhk828@naver.com>
2023-02-06 11:50:25 +0900

    23.02.06 예약/마이페이지 예약리스트

--- JSPFinalProject/src/main/java/com/sist/dao/ReserveDAO.java
+++ JSPFinalProject/src/main/java/com/sist/dao/ReserveDAO.java
@@ -1,108 +1,169 @@
 package com.sist.dao;
-
 import java.util.*;
 import java.sql.*;
 import com.sist.vo.*;
-
 public class ReserveDAO {
-	private Connection conn;
-	private PreparedStatement ps;
-	
-	public List<FoodVO> reserveFoodListData(String fd) {
-		List<FoodVO> list = new ArrayList<FoodVO>();
-		try {
-			conn = CreateConnnection.getConnection();
-			String sql = "SELECT fno, poster, name, address, tel, rownum "
-					+ "FROM food_location "
-					+ "WHERE rownum <= 50 AND type LIKE '%'||?||'%'";
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, fd);
-			ResultSet rs = ps.executeQuery();
-			while(rs.next()) {
-				
-				FoodVO vo = new FoodVO();
-				vo.setFno(rs.getInt(1));
-				String poster = rs.getString(2);
-				poster = poster.substring(0, poster.indexOf("^"));
-				poster = poster.replace("#", "&");
-				vo.setPoster(poster);
-				vo.setName(rs.getString(3));
-				String address = rs.getString(4);
-				address = address.substring(0, address.lastIndexOf("지")).trim();
-				vo.setAddress(address);
-				vo.setTel(rs.getString(5));
-				list.add(vo);
-			}
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
+  private Connection conn;
+  private PreparedStatement ps;
+  
+  public List<FoodVO> reserveFoodListData(String fd)
+  {
+	  List<FoodVO> list=new ArrayList<FoodVO>();
+	  try
+	  {
+		  conn=CreateConnnection.getConnection();
+		  String sql="SELECT fno,poster,name,address,tel,rownum "
+				    +"FROM food_location "
+				    +"WHERE rownum<=50 AND type LIKE '%'||?||'%'";
+		  ps=conn.prepareStatement(sql);
+		  ps.setString(1, fd);
+		  ResultSet rs=ps.executeQuery();
+		  while(rs.next())
+		  {
+			  FoodVO vo=new FoodVO();
+			  vo.setFno(rs.getInt(1));
+			  String poster=rs.getString(2);
+			  poster=poster.substring(0,poster.indexOf("^"));
+			  poster=poster.replace("#", "&");
+			  vo.setPoster(poster);
+			  vo.setName(rs.getString(3));
+			  String address=rs.getString(4);
+			  address=address.substring(0,address.lastIndexOf("지")).trim();
+			  vo.setAddress(address);
+			  vo.setTel(rs.getString(5));
+			  list.add(vo);
+		  }
+		  rs.close();
+	  }catch(Exception ex)
+	  {
+		  ex.printStackTrace();
+	  }
+	  finally
+	  {
+		  CreateConnnection.disConnection(conn, ps);
+	  }
+	  return list;
+  }
+  // 예약일 읽기
+  public String reserveDayData(int fno)
+  {
+	  String rdate="";
+	  try
+	  {
+		  conn=CreateConnnection.getConnection();
+		  String sql="SELECT reserve_day FROM food_location "
+				    +"WHERE fno=?";
+		  ps=conn.prepareStatement(sql);
+		  ps.setInt(1, fno);
+		  ResultSet rs=ps.executeQuery();
+		  rs.next();
+		  rdate=rs.getString(1);
+		  rs.close();
+	  }catch(Exception ex)
+	  {
+		  ex.printStackTrace();
+	  }
+	  finally
+	  {
+		  CreateConnnection.disConnection(conn, ps);
+	  }
+	  return rdate;
+  }
+  // 예약시간 
+  public String reserveTimeData(int dno)
+  {
+	  String times="";
+	  try
+	  {
+		  conn=CreateConnnection.getConnection();
+		  String sql="SELECT time FROM project_reserve_date "
+				    +"WHERE dno=?";
+		  ps=conn.prepareStatement(sql);
+		  ps.setInt(1, dno);
+		  ResultSet rs=ps.executeQuery();
+		  rs.next();
+		  times=rs.getString(1);
+		  rs.close();
+	  }catch(Exception ex)
+	  {
+		  ex.printStackTrace();
+	  }
+	  finally
+	  {
+		  CreateConnnection.disConnection(conn, ps);
+	  }
+	  return times;
+  }
+  public String reserveTimeRealData(int tno)
+  {
+	  String times="";
+	  try
+	  {
+		  conn=CreateConnnection.getConnection();
+		  String sql="SELECT time FROM project_reserve_time "
+				    +"WHERE tno=?";
+		  ps=conn.prepareStatement(sql);
+		  ps.setInt(1, tno);
+		  ResultSet rs=ps.executeQuery();
+		  rs.next();
+		  times=rs.getString(1);
+		  rs.close();
+	  }catch(Exception ex)
+	  {
+		  ex.printStackTrace();
+	  }
+	  finally
+	  {
+		  CreateConnnection.disConnection(conn, ps);
+	  }
+	  return times;
+  }
+  	// 예약 정보 저장
+  /*
+	    RNO        NOT NULL NUMBER       
+		FNO                 NUMBER       
+		ID                  VARCHAR2(20) 
+		RDATE      NOT NULL VARCHAR2(20) 
+		RTIME      NOT NULL VARCHAR2(20) 
+		INWON               NUMBER       
+		RESERVE_NO NOT NULL VARCHAR2(20) 
+		OK                  CHAR(1)      
+		REGDATE             DATE 
+   */
+  	public void reserveOk(ReserveVO vo) {
+  		try {
+  			conn=CreateConnnection.getConnection();
+  			String sql = "INSERT INTO project_reserve VALUES("
+  					+ "(SELECT NVL(MAX(rno)+1,1) FROM project_reserve),?,?,?,?,?,?,'n',SYSDATE)";
+  			ps = conn.prepareStatement(sql);
+  			ps.setInt(1, vo.getFno());
+  			ps.setString(2, vo.getId());
+  			ps.setString(3, vo.getRdate());
+  			ps.setString(4, vo.getRtime());
+  			ps.setInt(5, vo.getInwon());
+  			ps.setString(6, vo.getReserve_no());
+  			ps.executeUpdate();
+  		} catch (Exception e) {
+  			e.printStackTrace();
+  		} finally {
 			CreateConnnection.disConnection(conn, ps);
 		}
-		return list;
-	}
-	
-	// 예약일 읽기
-	public String reserveDayData(int fno) {
-		String rdate = "";
-		try {
-			conn = CreateConnnection.getConnection();
-			String sql = "SELECT reserve_day FROM good_location "
-					+ "WHERE fno = ?";
-			ps = conn.prepareStatement(sql);
-			ps.setInt(1, fno);
-			ResultSet rs = ps.executeQuery();
-			rs.next();
-			rdate = rs.getString(1);
-			rs.close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			CreateConnnection.disConnection(conn, ps);
-		}
-		return rdate;
-	}
-	// 예약시간
-	public String reserveTimeData(int dno) {
-		String times = "";
-		try {
-			conn = CreateConnnection.getConnection();
-			String sql = "SELECT time FROM project_reserve_date "
-					+ "WHERE dno=?";
-			ps = conn.prepareStatement(sql);
-			ps.setInt(1, dno);
-			ResultSet rs = ps.executeQuery();
-			rs.next();
-			times = rs.getString(1);
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			CreateConnnection.disConnection(conn, ps);
-		}
-		return times;
-	}
-	
-	public String reserveTimeRealData(int tno) {
-		String times = "";
-		try {
-			conn = CreateConnnection.getConnection();
-			String sql = "SELECT time FROM project_reserve_time "
-					+ "WHERE tno=?";
-			ps = conn.prepareStatement(sql);
-			ps.setInt(1, tno);
-			ResultSet rs = ps.executeQuery();
-			rs.next();
-			times = rs.getString(1);
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			CreateConnnection.disConnection(conn, ps);
-		}
-		return times;
-	}
-	// 예약 정보 저장
-	// 예약 승인
+  	}
+  	// 예약 정보를 마이페이지에서 읽기
+  	public List<ReserveVO> reserveMypageData(String id) {
+  		List<ReserveVO> list = new ArrayList<ReserveVO>();
+  		
+  		return list;
+  	}
+  	public List<ReserveVO> reserveAdminData(String id) {
+  		List<ReserveVO> list = new ArrayList<ReserveVO>();
+  		
+  		return list;
+  	}
+  	// 예약 승인 
 }
+
+
+
+
+
